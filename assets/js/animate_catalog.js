38c0c1a8e5e26382985c1cebd8c6d8596ae1d8b9
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-01-19 18:55:12 +0300

    сделал анимацию

--- assets/js/animate_catalog.js
+++ assets/js/animate_catalog.js
@@ -1,32 +1,46 @@
-const productBox = document.querySelectorAll(".product");
-productBox.forEach((element) => {
-  let btnBuy = element.childNodes[3].childNodes[5];
-  btnBuy.addEventListener("click", () => {
+let product = document.querySelectorAll(".product");
+product.forEach((e) => {
+  let btn = e.childNodes[3].childNodes[5];
+  btn.addEventListener("click", () => {
+    function getCoords() {
+      let cart = document.querySelector(".header__menu-cart");
+      let img = e.childNodes[1];
+      let image = e.childNodes[1].childNodes[1].cloneNode();
+      let imgPos = img.getBoundingClientRect();
+      let cartPlace = cart.getBoundingClientRect();
+      let body = document.querySelector(".header").getBoundingClientRect();
+      let topImage = body.top + imgPos.top;
+      // console.log(body.top);
+      image.style.position = "absolute";
+      image.style.top = img.offsetTop + "px";
+      image.style.left = img.offsetLeft + "px";
+      image.style.zIndex = 999999;
+      image.style.width = "281px";
+      let top = cart.offsetTop;
+      let left = cart.offsetLeft + "px";
+      // let finishLeft = window.pageYOffset + left + "px";
+      let finishTop = window.pageYOffset - top - 50 + "px";
+      // console.log(finishLeft);
+      console.log(finishTop);
+      document.body.appendChild(image);
+      image.animate(
+        [
+          {
+            top: finishTop,
+            left: left,
+            transform: "scale(0.2)",
+            // transform: `translate( ${finishLeft}px, ${finishTop}px) scale(0.5)`,
+          },
+        ],
+        {
+          duration: 800,
+        }
+      );
+      setTimeout(remImg, 800);
+      function remImg() {
+        image.remove();
+      }
+    }
     getCoords();
   });
 });
-function getCoords() {
-  let cart = document.querySelector(".header__menu-cart");
-  let imgDef = element.childNodes[1].childNodes[1];
-  console.log(imgDef);
-  let image = element.childNodes[1].childNodes[1].cloneNode();
-  image.style.position = "absolute";
-  image.style.zIndex = "99";
-  image.style.width = "284px";
-  image.style.left = imgDef.offsetLeft + "px";
-  image.style.top = imgDef.offsetTop + "px";
-  console.log(image.style.left);
-  console.log(cart.offsetTop);
-  document.querySelector("main").append(image);
-  image.animate(
-    [
-      { top: cart.offsetTop + "px" },
-      { left: cart.offsetLeft + "px" },
-      { transform: "scale(0.5)" },
-      { opacity: 0 },
-    ],
-    {
-      duration: 1000,
-    }
-  );
-}
