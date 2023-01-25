bc956f786d52cd09891e29035dd331a04299c2e2
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-01-25 13:53:49 +0300

    добавлена анимация на странице skvazhina

--- assets/js/animate_skvazhina.js
+++ assets/js/animate_skvazhina.js
@@ -11,36 +11,38 @@ const blockHeight = document.querySelector(
 const blockHeightSecond = document.querySelector(
   ".textblock-wrap-container:nth-child(2)"
 );
-let topBlock = blockHeight.getBoundingClientRect().top;
-let bottomBlock = blockHeight.getBoundingClientRect().bottom;
+let topBlock = blockImage.getBoundingClientRect().top;
+let bottomBlock = blockImage.getBoundingClientRect().bottom;
+let topBlockSecond = blockImageSecond.getBoundingClientRect().top;
+let bottomBlockSecond = blockImageSecond.getBoundingClientRect().bottom;
 // let topBlock1 = topBlock;
-
-window.addEventListener("scroll", scrollFirst);
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > bottomBlock) {
-    window.removeEventListener("scroll", scrollFirst);
-  }
-});
-
-window.addEventListener("scroll", scrollSecond);
-function scrollFirst() {
-  if (window.pageYOffset > blockImage.offsetTop - 70) {
+document.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("scroll", () => {
+    let scrollY = window.scrollY;
+    if (scrollY > topBlock - 100 && scrollY < bottomBlock) {
+      scrollFirst();
+      blockImage.classList.add("fixed");
+    } else {
+      blockImage.classList.remove("fixed");
+    }
+    if (scrollY > topBlockSecond - 100 && scrollY < bottomBlockSecond) {
+      scrollSecond();
+      blockImageSecond.classList.add("fixed2");
+    } else {
+      blockImageSecond.classList.remove("fixed2");
+    }
+  });
+  function scrollFirst() {
     let body = window.document.body.offsetWidth;
     let wrap = document.querySelector(".textblock-wrap").offsetWidth;
     let sum = (body - wrap) / 2;
     let sum2 = wrap - blockImage.offsetWidth;
-    blockImage.style.position = "fixed";
-    blockImage.style.top = "60px";
     blockImage.style.left = sum + sum2 - 50 + "px";
   }
-}
-function scrollSecond() {
-  if (window.pageYOffset > blockImageSecond.offsetTop - 80) {
+  function scrollSecond() {
     let body = window.document.body.offsetWidth;
     let wrap = document.querySelector(".textblock-wrap").offsetWidth;
     let sum = (body - wrap) / 2;
-    blockImageSecond.style.position = "fixed";
-    blockImageSecond.style.top = "90px";
     blockImageSecond.style.left = sum + 50 + "px";
   }
-}
+});
