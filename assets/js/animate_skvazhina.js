3965b3419b6f5939e18b10ca67f07d116bc22828
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-01-25 14:07:02 +0300

    добавлена анимация на странице skvazhina

--- assets/js/animate_skvazhina.js
+++ assets/js/animate_skvazhina.js
@@ -17,32 +17,34 @@ let topBlockSecond = blockImageSecond.getBoundingClientRect().top;
 let bottomBlockSecond = blockImageSecond.getBoundingClientRect().bottom;
 // let topBlock1 = topBlock;
 document.addEventListener("DOMContentLoaded", () => {
-  window.addEventListener("scroll", () => {
-    let scrollY = window.scrollY;
-    if (scrollY > topBlock - 100 && scrollY < bottomBlock) {
-      scrollFirst();
-      blockImage.classList.add("fixed");
-    } else {
-      blockImage.classList.remove("fixed");
+  if (window.innerWidth > 1200) {
+    window.addEventListener("scroll", () => {
+      let scrollY = window.scrollY;
+      if (scrollY > topBlock - 100 && scrollY < bottomBlock) {
+        scrollFirst();
+        blockImage.classList.add("fixed");
+      } else {
+        blockImage.classList.remove("fixed");
+      }
+      if (scrollY > topBlockSecond - 100 && scrollY < bottomBlockSecond) {
+        scrollSecond();
+        blockImageSecond.classList.add("fixed2");
+      } else {
+        blockImageSecond.classList.remove("fixed2");
+      }
+    });
+    function scrollFirst() {
+      let body = window.document.body.offsetWidth;
+      let wrap = document.querySelector(".textblock-wrap").offsetWidth;
+      let sum = (body - wrap) / 2;
+      let sum2 = wrap - blockImage.offsetWidth;
+      blockImage.style.left = sum + sum2 - 50 + "px";
     }
-    if (scrollY > topBlockSecond - 100 && scrollY < bottomBlockSecond) {
-      scrollSecond();
-      blockImageSecond.classList.add("fixed2");
-    } else {
-      blockImageSecond.classList.remove("fixed2");
+    function scrollSecond() {
+      let body = window.document.body.offsetWidth;
+      let wrap = document.querySelector(".textblock-wrap").offsetWidth;
+      let sum = (body - wrap) / 2;
+      blockImageSecond.style.left = sum + 50 + "px";
     }
-  });
-  function scrollFirst() {
-    let body = window.document.body.offsetWidth;
-    let wrap = document.querySelector(".textblock-wrap").offsetWidth;
-    let sum = (body - wrap) / 2;
-    let sum2 = wrap - blockImage.offsetWidth;
-    blockImage.style.left = sum + sum2 - 50 + "px";
-  }
-  function scrollSecond() {
-    let body = window.document.body.offsetWidth;
-    let wrap = document.querySelector(".textblock-wrap").offsetWidth;
-    let sum = (body - wrap) / 2;
-    blockImageSecond.style.left = sum + 50 + "px";
   }
 });
