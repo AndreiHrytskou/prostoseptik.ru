696ac3e11e9c403390477ecccaeb79770b22a77c
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-01-29 15:29:46 +0300

    добавил анимацию на странице продукта

--- assets/js/single-product.js
+++ assets/js/single-product.js
@@ -4,3 +4,41 @@ sub.addEventListener("click", (e) => {
   e.preventDefault();
   createThanks();
 });
+buy.addEventListener("click", () => {
+  const spanBack = document.createElement("div");
+  const modalBlock = document.createElement("div");
+  const img = document.createElement("img");
+  const text = document.createElement("p");
+  const toCart = document.createElement("a");
+  const toCatalog = document.createElement("a");
+  const remove = document.createElement("span");
+  modalBlock.className = "modal modal__active modal__product";
+  img.className = "modal__img";
+  img.src = "assets/img/doneicon.png";
+  text.className = "title";
+  toCart.href = "./cart.html";
+  toCatalog.href = "./catalog.html";
+  toCatalog.className = "toCatalog";
+  remove.className = "remove";
+  document.body.append(modalBlock);
+  document.body.append(spanBack);
+  modalBlock.append(img);
+  modalBlock.append(text);
+  modalBlock.append(toCart);
+  modalBlock.append(toCatalog);
+  modalBlock.append(remove);
+  toCart.append("перейти в корзину");
+  toCatalog.append("вернуться в каталог");
+  remove.append("x");
+  toCart.className = "btn";
+  text.append("Товар добавлен в корзину");
+  spanBack.classList.toggle("background__white");
+  spanBack.addEventListener("click", () => {
+    spanBack.classList.toggle("background__white");
+    modalBlock.remove();
+  });
+  remove.addEventListener("click", () => {
+    spanBack.classList.toggle("background__white");
+    modalBlock.remove();
+  });
+});
