104728dad12d5a2762b9a90b83c382a81f20d75a
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-01-30 21:31:15 +0300

    поправил скорость анимации на главной первый банер и изменил цвет номера телефона и почты

--- assets/js/animation_headPage.js
+++ assets/js/animation_headPage.js
@@ -1,13 +1,13 @@
-window.addEventListener("load", () => {
-  setTimeout(loadBlock, 800);
-  function loadBlock() {
-    let block = document.querySelectorAll(".block-bottom-item");
-    block.forEach((e) => {
-      e.style.transform = "scale(1)";
-      e.style.transition = "0.5s";
-    });
-  }
-});
+// window.addEventListener("load", () => {
+setTimeout(loadBlock, 800);
+function loadBlock() {
+  let block = document.querySelectorAll(".block-bottom-item");
+  block.forEach((e) => {
+    e.style.transform = "scale(1)";
+    e.style.transition = "0.5s";
+  });
+}
+// });
 
 const review = document.querySelector(".review");
 window.addEventListener("scroll", () => {
