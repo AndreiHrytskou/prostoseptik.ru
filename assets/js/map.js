bf588daf8c8d8c439dfc511a1bbc265ac4591232
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-01-30 23:38:04 +0300

    добавил кнопки и модалки на карту сайта

--- assets/js/map.js
+++ assets/js/map.js
@@ -70,3 +70,45 @@ function createThank() {
     div.classList.toggle("white");
   });
 }
+// contacts
+
+const inputBtn = document.querySelector(".form-btn");
+
+inputBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  createThanks();
+});
+function createThanks() {
+  const div = document.createElement("div");
+  const thanks = document.createElement("div");
+  const thanksTitle = document.createElement("p");
+  const thanksText = document.createElement("p");
+  const thanksBtn = document.createElement("button");
+  div.classList.toggle("div-hide");
+  thanks.className = "thanks";
+  thanksTitle.className = "title center";
+  thanksText.className = "modal-text";
+  thanksBtn.className = "mobal__btn";
+  document.body.append(thanks);
+  document.body.append(div);
+  thanks.append(thanksTitle);
+  thanks.append(thanksText);
+  thanks.append(thanksBtn);
+  thanksTitle.append("Спасибо за Ваше обращение!");
+  thanksText.append("В течение дня мы с Вами свяжемся");
+  thanksBtn.append("Продолжить");
+  thanksBtn.addEventListener("click", () => {
+    thanks.remove();
+    div.remove();
+  });
+  div.addEventListener("click", () => {
+    thanks.remove();
+    div.classList.toggle("div-hide");
+  });
+}
+// checkout
+let order = document.querySelector(".btn-order");
+order.addEventListener("click", (e) => {
+  e.preventDefault();
+  createThank();
+});
