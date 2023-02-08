273f20b98eacc2aac44aeffc791df2a69ff59463
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-02-08 10:25:54 +0300

    поправил главную. каталог добавил текстовый блок. завод воды исправил swiper

--- assets/js/zavod.js
+++ assets/js/zavod.js
@@ -1,11 +1,16 @@
 var swiper = new Swiper(".mySwiper9", {
   spaceBetween: 21,
   slidesPerView: 3,
+
   freeMode: true,
   watchSlidesProgress: true,
 });
 var swiper2 = new Swiper(".mySwiper10", {
   spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -22,6 +27,10 @@ var swiper = new Swiper(".mySwiper12", {
 });
 var swiper2 = new Swiper(".mySwiper11", {
   spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination1",
+    clickable: true,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -38,6 +47,10 @@ var swiper = new Swiper(".mySwiper14", {
 });
 var swiper2 = new Swiper(".mySwiper13", {
   spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination2",
+    clickable: true,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
