09759578b9950874cd87fdd944c6555f2ec829a5
Andrei Hrytskou <andreihrytskou@gmail.com>
2023-02-14 12:27:19 +0300

    изменил video.js

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,18 +1,21 @@
 let video = document.querySelector(".video-bottom__item");
 let videoImg = document.querySelector(".video-bottom__item-img");
-let videoFrame = document.querySelector(".video-bottom__item iframe");
+const iframe = document.createElement("iframe");
+video.append(iframe);
+
 let heightImg;
 video.addEventListener("click", (e) => {
   e.preventDefault();
+  iframe.className = "video__frame";
   let videoSrc = video.href;
   let videoId = videoSrc.substr(17) + "?rel=0&autoplay=1";
   videoImg.style.display = "none";
   let link = "https://www.youtube.com/embed/" + videoId;
-  videoFrame.src = link;
-  videoFrame.style.display = "block";
-  videoFrame.setAttribute("allow", "autoplay");
+  iframe.src = link;
+  iframe.style.display = "block";
+  iframe.setAttribute("allow", "autoplay");
 });
 videoImg.onload = function () {
   heightImg = this.height;
-  videoFrame.style.height = `${heightImg}px`;
+  iframe.style.height = `${heightImg}px`;
 };
